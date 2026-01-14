0948c8e4135a83a5396ea3559faeed14ad053d2f
jameszhang-China <785755989@qq.com>
2026-01-14 23:08:47 +0800

    Delete train.py

--- train.py
+++
@@ -1,8 +0,0 @@
-from libs.engine.model import Classification
-
-# model = Classification(model='cfg/resnet18.yaml', hyper=False)
-# model.train()
-
-model = Classification(model='best.pt', hyper=False)
-model.predict()
-
